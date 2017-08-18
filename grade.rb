1d63de04dca713f6adf99641716e0fc92a8da65c
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-08-18 16:29:52 -0400

    add assignment

---
+++ grade.rb
@@ -0,0 +1,28 @@
+# Define a method that accepts a percentage as an argument and returns a letter grade (A+, A, A-, B+, etc) for that percentage.
+#
+# Prompt your user to enter a percentage and display a message showing them the equivalent letter grade.
+
+
+def grade(percentage)
+
+end
+
+
+puts "Enter a percentage"
+gets.to_i
+
+
+
+A+ >= 97 && <= 100
+A >= 93 && <= 96
+A- >= 90 && <= 92
+B+ >= 87 && <= 89
+B >= 83 && <= 86
+B- >= 80 && <= 82
+C+ >= 77 && <= 79
+C >= 73 && <= 76
+C- >= 70 && <= 72
+D+ >= 67 && <= 69
+D >= 63 && <= 66
+D- >= 60 && <= 62
+F < 60 && >=0
