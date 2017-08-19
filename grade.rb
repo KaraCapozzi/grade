2e7a1fd7828676649ab404e1594c157f6657d44d
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-08-19 00:44:43 -0400

    finished grade assignment

--- grade.rb
+++ grade.rb
@@ -2,27 +2,53 @@
 #
 # Prompt your user to enter a percentage and display a message showing them the equivalent letter grade.
 
-
 def grade(percentage)
-
+  if percentage >= 97 && percentage <= 100
+   puts "A+"
+  elsif percentage >= 93 && percentage <= 96
+   puts "A"
+  elsif percentage >= 90 && percentage <= 92
+   puts "A-"
+  elsif percentage >= 87 && percentage <= 89
+   puts "B+"
+  elsif percentage >= 83 && percentage <= 86
+    puts "B"
+  elsif percentage >= 80 && percentage <= 82
+    puts "B-"
+  elsif percentage >= 77 && percentage <= 79
+    puts "C+"
+  elsif percentage >= 73 && percentage <= 76
+    puts "C"
+  elsif percentage >= 70 && percentage <= 72
+    puts "C-"
+  elsif percentage >= 67 && percentage <= 69
+    puts "D+"
+  elsif percentage >= 63 && percentage <= 66
+    puts "D"
+  elsif percentage >= 60 && percentage <= 62
+    puts "D-"
+  elsif percentage < 60 && percentage >= 0
+    puts "F"
+  end
 end
 
-
 puts "Enter a percentage"
-gets.to_i
+percentage = gets.to_i
+grade(percentage)
+#i forgot how to pass something through a method or more specifially how to call a method just syntax
 
 
-
-A+ >= 97 && <= 100
-A >= 93 && <= 96
-A- >= 90 && <= 92
-B+ >= 87 && <= 89
-B >= 83 && <= 86
-B- >= 80 && <= 82
-C+ >= 77 && <= 79
-C >= 73 && <= 76
-C- >= 70 && <= 72
-D+ >= 67 && <= 69
-D >= 63 && <= 66
-D- >= 60 && <= 62
-F < 60 && >=0
+#percentage conversions:
+# A+ >= 97 && <= 100
+# A >= 93 && <= 96
+# A- >= 90 && <= 92
+# B+ >= 87 && <= 89
+# B >= 83 && <= 86
+# B- >= 80 && <= 82
+# C+ >= 77 && <= 79
+# # C >= 73 && <= 76
+# # C- >= 70 && <= 72
+# # D+ >= 67 && <= 69
+# # D >= 63 && <= 66
+# # D- >= 60 && <= 62
+# # F < 60 && >= 0
